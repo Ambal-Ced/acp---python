70dffe24e7974d07e59eb433fd3da619e1ee727c
Ambal-Ced <147784891+Ambal-Ced@users.noreply.github.com>
2023-10-13 10:47:54 +0800

    Add files via upload

---
+++ LabEx1.py
@@ -0,0 +1,18 @@
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
+
+print("Addition")
+sum = num1 + num2
+print(num1, "+",num2, "=", sum)
+
+print("Subtraction")
+dif= num1 - num2
+print(num1, "-",num2, "=", dif)
+
+print("Multiplication")
+pro= num1*num2
+print(num1, "*",num2, "=", pro)
+
+print("Division")
+quo= num1/num2
+print(num1, "/",num2, "=", quo)
